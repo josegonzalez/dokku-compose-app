4612d8adda2915a9c5f32e00d058f2c8eeaa4d6b
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2015-10-23 06:08:11 -0400

    Add proper support for environment variables

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+import os
+
 from flask import Flask
 from redis import Redis
+from urlparse import urlparse
+
+dsn = urlparse(os.getenv('REDIS_URL', 'redis://localhost:6379/0'))
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+redis = Redis(host=dsn.hostname, port=dsn.port)
+
 
 @app.route('/')
 def hello():
