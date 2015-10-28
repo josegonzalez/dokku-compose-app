9ba26ce5332f1d54a1e72e510d3cec474bedbc13
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2015-10-28 14:25:02 -0400

    Slightly better python code

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
+import flask
 import os
+import redis
+import urlparse
 
-from flask import Flask
-from redis import Redis
-from urlparse import urlparse
-
-dsn = urlparse(os.getenv('REDIS_URL', 'redis://localhost:6379/0'))
-
-app = Flask(__name__)
-redis = Redis(host=dsn.hostname, port=dsn.port)
+dsn = urlparse.urlparse(os.getenv('REDIS_URL', 'redis://localhost:6379/0'))
+debug = os.getenv('DEBUG', '0').lower()
+debug = True if debug in ['1', 'true', 'on'] else False
+app = flask.Flask(__name__)
+redis_client = redis.Redis(host=dsn.hostname, port=dsn.port)
 
 
 @app.route('/')
 def hello():
-    redis.incr('hits')
-    return 'Hello World! I have been seen %s times.' % redis.get('hits')
+    redis_client.incr('hits')
+    return 'Hello World! I have been seen {0} times.'.format(
+        redis_client.get('hits')
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
